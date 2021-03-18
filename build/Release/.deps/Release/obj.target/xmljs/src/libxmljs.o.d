ac1bfe1005094e7a5701f58c4ffd91ea67a963bc
Erich Wittenbeck <erich.wittenbeck@stroeer.de>
2021-03-18 15:33:44 +0100

    ci: store binary in package

---
+++ build/Release/.deps/Release/obj.target/xmljs/src/libxmljs.o.d
@@ -0,0 +1,115 @@
+cmd_Release/obj.target/xmljs/src/libxmljs.o := g++ '-DNODE_GYP_MODULE_NAME=xmljs' '-DUSING_UV_SHARED=1' '-DUSING_V8_SHARED=1' '-DV8_DEPRECATION_WARNINGS=1' '-DV8_DEPRECATION_WARNINGS' '-DV8_IMMINENT_DEPRECATION_WARNINGS' '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-D__STDC_FORMAT_MACROS' '-DOPENSSL_NO_PINSHARED' '-DOPENSSL_THREADS' '-DLIBXML_XINCLUDE_ENABLED' -I/github/home/.cache/node-gyp/12.19.0/include/node -I/github/home/.cache/node-gyp/12.19.0/src -I/github/home/.cache/node-gyp/12.19.0/deps/openssl/config -I/github/home/.cache/node-gyp/12.19.0/deps/openssl/openssl/include -I/github/home/.cache/node-gyp/12.19.0/deps/uv/include -I/github/home/.cache/node-gyp/12.19.0/deps/zlib -I/github/home/.cache/node-gyp/12.19.0/deps/v8/include -I../vendor/libxml/include -I../node_modules/nan  -fPIC -pthread -Wall -Wextra -Wno-unused-parameter -m64 -Wall -O3 -fno-omit-frame-pointer -fno-rtti -fno-exceptions -std=gnu++1y -MMD -MF ./Release/.deps/Release/obj.target/xmljs/src/libxmljs.o.d.raw   -c -o Release/obj.target/xmljs/src/libxmljs.o ../src/libxmljs.cc
+Release/obj.target/xmljs/src/libxmljs.o: ../src/libxmljs.cc \
+ /github/home/.cache/node-gyp/12.19.0/include/node/v8.h \
+ /github/home/.cache/node-gyp/12.19.0/include/node/v8-internal.h \
+ /github/home/.cache/node-gyp/12.19.0/include/node/v8-version.h \
+ /github/home/.cache/node-gyp/12.19.0/include/node/v8config.h \
+ ../vendor/libxml/include/libxml/xmlmemory.h \
+ ../vendor/libxml/include/libxml/xmlversion.h \
+ ../vendor/libxml/include/libxml/xmlexports.h \
+ ../vendor/libxml/include/libxml/threads.h \
+ ../vendor/libxml/include/libxml/globals.h \
+ ../vendor/libxml/include/libxml/parser.h \
+ ../vendor/libxml/include/libxml/tree.h \
+ ../vendor/libxml/include/libxml/xmlstring.h \
+ ../vendor/libxml/include/libxml/xmlregexp.h \
+ ../vendor/libxml/include/libxml/dict.h \
+ ../vendor/libxml/include/libxml/hash.h \
+ ../vendor/libxml/include/libxml/valid.h \
+ ../vendor/libxml/include/libxml/xmlerror.h \
+ ../vendor/libxml/include/libxml/list.h \
+ ../vendor/libxml/include/libxml/xmlautomata.h \
+ ../vendor/libxml/include/libxml/entities.h \
+ ../vendor/libxml/include/libxml/encoding.h \
+ ../vendor/libxml/include/libxml/xmlIO.h \
+ ../vendor/libxml/include/libxml/SAX2.h \
+ ../vendor/libxml/include/libxml/xlink.h ../src/libxmljs.h \
+ ../node_modules/nan/nan.h \
+ /github/home/.cache/node-gyp/12.19.0/include/node/node_version.h \
+ /github/home/.cache/node-gyp/12.19.0/include/node/uv.h \
+ /github/home/.cache/node-gyp/12.19.0/include/node/uv/errno.h \
+ /github/home/.cache/node-gyp/12.19.0/include/node/uv/version.h \
+ /github/home/.cache/node-gyp/12.19.0/include/node/uv/unix.h \
+ /github/home/.cache/node-gyp/12.19.0/include/node/uv/threadpool.h \
+ /github/home/.cache/node-gyp/12.19.0/include/node/uv/linux.h \
+ /github/home/.cache/node-gyp/12.19.0/include/node/node.h \
+ /github/home/.cache/node-gyp/12.19.0/include/node/v8.h \
+ /github/home/.cache/node-gyp/12.19.0/include/node/v8-platform.h \
+ /github/home/.cache/node-gyp/12.19.0/include/node/node_version.h \
+ /github/home/.cache/node-gyp/12.19.0/include/node/node_buffer.h \
+ /github/home/.cache/node-gyp/12.19.0/include/node/node.h \
+ /github/home/.cache/node-gyp/12.19.0/include/node/node_object_wrap.h \
+ ../node_modules/nan/nan_callbacks.h \
+ ../node_modules/nan/nan_callbacks_12_inl.h \
+ ../node_modules/nan/nan_maybe_43_inl.h \
+ ../node_modules/nan/nan_converters.h \
+ ../node_modules/nan/nan_converters_43_inl.h \
+ ../node_modules/nan/nan_new.h \
+ ../node_modules/nan/nan_implementation_12_inl.h \
+ ../node_modules/nan/nan_persistent_12_inl.h \
+ ../node_modules/nan/nan_weak.h ../node_modules/nan/nan_object_wrap.h \
+ ../node_modules/nan/nan_private.h \
+ ../node_modules/nan/nan_typedarray_contents.h \
+ ../node_modules/nan/nan_json.h ../src/xml_document.h \
+ ../src/xml_namespace.h ../src/xml_node.h ../src/xml_sax_parser.h \
+ ../src/xml_textwriter.h ../vendor/libxml/include/libxml/xmlwriter.h
+../src/libxmljs.cc:
+/github/home/.cache/node-gyp/12.19.0/include/node/v8.h:
+/github/home/.cache/node-gyp/12.19.0/include/node/v8-internal.h:
+/github/home/.cache/node-gyp/12.19.0/include/node/v8-version.h:
+/github/home/.cache/node-gyp/12.19.0/include/node/v8config.h:
+../vendor/libxml/include/libxml/xmlmemory.h:
+../vendor/libxml/include/libxml/xmlversion.h:
+../vendor/libxml/include/libxml/xmlexports.h:
+../vendor/libxml/include/libxml/threads.h:
+../vendor/libxml/include/libxml/globals.h:
+../vendor/libxml/include/libxml/parser.h:
+../vendor/libxml/include/libxml/tree.h:
+../vendor/libxml/include/libxml/xmlstring.h:
+../vendor/libxml/include/libxml/xmlregexp.h:
+../vendor/libxml/include/libxml/dict.h:
+../vendor/libxml/include/libxml/hash.h:
+../vendor/libxml/include/libxml/valid.h:
+../vendor/libxml/include/libxml/xmlerror.h:
+../vendor/libxml/include/libxml/list.h:
+../vendor/libxml/include/libxml/xmlautomata.h:
+../vendor/libxml/include/libxml/entities.h:
+../vendor/libxml/include/libxml/encoding.h:
+../vendor/libxml/include/libxml/xmlIO.h:
+../vendor/libxml/include/libxml/SAX2.h:
+../vendor/libxml/include/libxml/xlink.h:
+../src/libxmljs.h:
+../node_modules/nan/nan.h:
+/github/home/.cache/node-gyp/12.19.0/include/node/node_version.h:
+/github/home/.cache/node-gyp/12.19.0/include/node/uv.h:
+/github/home/.cache/node-gyp/12.19.0/include/node/uv/errno.h:
+/github/home/.cache/node-gyp/12.19.0/include/node/uv/version.h:
+/github/home/.cache/node-gyp/12.19.0/include/node/uv/unix.h:
+/github/home/.cache/node-gyp/12.19.0/include/node/uv/threadpool.h:
+/github/home/.cache/node-gyp/12.19.0/include/node/uv/linux.h:
+/github/home/.cache/node-gyp/12.19.0/include/node/node.h:
+/github/home/.cache/node-gyp/12.19.0/include/node/v8.h:
+/github/home/.cache/node-gyp/12.19.0/include/node/v8-platform.h:
+/github/home/.cache/node-gyp/12.19.0/include/node/node_version.h:
+/github/home/.cache/node-gyp/12.19.0/include/node/node_buffer.h:
+/github/home/.cache/node-gyp/12.19.0/include/node/node.h:
+/github/home/.cache/node-gyp/12.19.0/include/node/node_object_wrap.h:
+../node_modules/nan/nan_callbacks.h:
+../node_modules/nan/nan_callbacks_12_inl.h:
+../node_modules/nan/nan_maybe_43_inl.h:
+../node_modules/nan/nan_converters.h:
+../node_modules/nan/nan_converters_43_inl.h:
+../node_modules/nan/nan_new.h:
+../node_modules/nan/nan_implementation_12_inl.h:
+../node_modules/nan/nan_persistent_12_inl.h:
+../node_modules/nan/nan_weak.h:
+../node_modules/nan/nan_object_wrap.h:
+../node_modules/nan/nan_private.h:
+../node_modules/nan/nan_typedarray_contents.h:
+../node_modules/nan/nan_json.h:
+../src/xml_document.h:
+../src/xml_namespace.h:
+../src/xml_node.h:
+../src/xml_sax_parser.h:
+../src/xml_textwriter.h:
+../vendor/libxml/include/libxml/xmlwriter.h:
